05adf91ff1c163c4166594706de6d1a81f5944ed
wxnacy <wxnacy@gmail.com>
2020-10-15 03:10:46 +0000

    commit F: 增加控制音量的 hotkey

---
+++ volume.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Author: wxnacy(wxnacy@gmail.com)
+# Description:
+
+curr_vol=$(osascript -e 'output volume of (get volume settings)')
+
+# echo $curr_vol
+if [[ $1 == 'sub' && $curr_vol > 0 ]]
+then
+    curr_vol=$[ curr_vol - 10 ]
+fi
+
+if [[ $1 == 'add' ]]
+then
+    curr_vol=$[ curr_vol + 10 ]
+fi
+
+
+osascript -e "set volume output volume $curr_vol"
+echo $curr_vol
